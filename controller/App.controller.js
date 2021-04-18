58f7a945b26387e8548f418ffde791c59e2926ba
haritha2528 <devipinni21@gmail.com>
2021-04-18 22:08:28 +0000

    Message Toast

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -1,15 +1,19 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
 ],
 	/**
 	 * @param {typeof sap.ui.core.mvc.Controller} Controller
 	 */
-	function (Controller) {
+	function (Controller,MessageToast) {
 		"use strict";
 
 		return Controller.extend("ns.quickstartproject1.controller.App", {
 			onInit: function () {
 
-			}
+            },
+            onreadybtn: function(){
+                MessageToast.show("Welcome to UI5 World!");
+            }
 		});
 	});
